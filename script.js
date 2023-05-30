102b616b0fcb0652cbd192d35c9fe7080e23cdbb
Ezeh Godwin <ezehgodwin3942@gmail.com>
2023-05-30 02:10:06 -0700

    fixed up some errors

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-function getvalue(){
-    let text = document.getElementById("email").Value;
-    console.log(text);
-}
-
+ retur = document.getElementById("memo");
+let error = document.getElementById("demo");
 let man = document.getElementById("man");
 let jsfun = document.getElementById("jsfun");
 
 
-document.getElementById("btn").addEventListener("click", function(){
-   man.classList.toggle("display1");
-   jsfun.classList.toggle("display2");
-});
+function getValue() {
+  var emailfil = document.getElementById("email").value;
+  if (emailfil === "" ) {
+     error.innerHTML = "please enter valid email";
+  }else {
+    man.classList.toggle("display1");
+    jsfun.classList.toggle("display2");
+    retur.innerHTML = `A confermation email has been sent to ${emailfil} please open it and click the button inside to conferm your subscription`; 
+
+  }
+}
 
 document.getElementById("move").addEventListener("click", function(){
     man.classList.toggle("display3");
